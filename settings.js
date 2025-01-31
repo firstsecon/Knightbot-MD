8330020df62687d2dd72f6e7b1d5ad5e9a48b314
firstsecon <maswad289@gmail.com>
2025-02-01 01:07:55 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'M ASWAD',
+  author: 'M ASWAD',
+  botName: "M ASWAD BOT",
+  botOwner: 'M ASWAD', // Your name
+  ownerNumber: '923419159670', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
